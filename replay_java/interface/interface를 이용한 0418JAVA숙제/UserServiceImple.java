fbf7d9c9d1a24b0555c4b5850bc6fb394ad2d42f
RyuGeun <h46344@naver.com>
2022-04-20 16:21:56 +0900

    feat : 내용수정

--- replay_java/interface/interface를 이용한 0418JAVA숙제/UserServiceImple.java
+++ replay_java/interface/interface를 이용한 0418JAVA숙제/UserServiceImple.java
@@ -3,6 +3,16 @@ package user;
 import java.util.List;
 
 public interface UserServiceImple {
+    // Window -> preferences -> java -> Code Stlye -> Code Templates -> Comments
+	// 코멘트 달기 사용목적 : 누가 언제 메소드 or 클래스를 만들었는지 추적 하기 위해서
+	// 단축키 Alt + Shift + j
+	/**
+	 * @param list
+	 * @param dname
+	 * @return List
+	 * @date : 2022. 4. 20.
+	 * @author : geunhwan Ryu
+	 */
 	public List<UserVO> updateSal(List<UserVO> list, String dname);
 	public List<UserVO> updateLv(List<UserVO> list);
 	public List<UserVO> deleteUser(List<UserVO> list, int lv);
