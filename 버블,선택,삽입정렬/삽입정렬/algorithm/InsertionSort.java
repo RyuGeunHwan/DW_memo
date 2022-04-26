52066cc492ad6de81938fe52c6a5e740b0cfd42c
RyuGeun <h46344@naver.com>
2022-04-26 17:47:51 +0900

    feat : 손코딩의 위력, 이해 쌉가능

--- 버블,선택,삽입정렬/삽입정렬/algorithm/InsertionSort.java
+++ 버블,선택,삽입정렬/삽입정렬/algorithm/InsertionSort.java
@@ -11,7 +11,7 @@
 package 삽입정렬;
 // 버블정렬, 선택정렬, 삽입정렬 중에 성능이 제일 좋은 정렬은 삽입정렬이다.
 
-public class InsertionSort {
+public class 삽입정렬 {
 	
 	public static void main(String[] args) {
         // 이중 for문 사용! 
@@ -20,24 +20,50 @@ public class InsertionSort {
 		int len = array.length;
 		int temp;
 		int i,j;
-		for(i=0; i<len; i++) {
+		for(i=1; i<len; i++) {
 			j=i;
 			while(j>0 && array[j-1]>array[j]) {
 				temp = array[j];
 				array[j] = array[j-1];
 				array[j-1] = temp;
-				j= j-1;
+				j--;
 			}
 		}
 		for(i=0; i<len; i++) {
 			System.out.println(array[i]);
 		}
 	//풀이
-	//
+	// i는 계속 증감하지만 while문안에 두가지 조건을 다 갖춰야 하기 때문에
+	// j가 while문을 돌때 마다 1씩 작아지지만 0보다 크고 
+	// [j-1]의 index가 [j]의 index보다 크다면 while문은 계속 반복실행
+	
+	// for문이 돌아 i가 증감할 때 ++회전수;
+		
+	// for문 1회전 array[] = {5,8,6,2,4}
+		
+	// for문 2회전 array[] = {5,6,8,2,4}
+	// for문 2회전 까지는 while문 1번씩만 실행 
+		
+	// for문 3회전 while문 첫번째 실행했을경우 array[]={5,6,2,8,4}
+	// for문 3회전 while문 두번째 실행했을경우 array[]={5,2,6,8,4}
+	// for문 3회전 while문 세번째 실행했을경우 array[]={2,5,6,8,4}
+	// for문 3회전 while문 세번째 실행했을경우 while문의 조건이 false이므로 break;
+		//(2가 최솟값이기 때문에 j는 감소하여 0이 되므로 전자 조건부(j>0)가 맞지않음.
+	// for문 3회전 while문 전체 실행 후 -> array[]={2,5,6,8,4}
+	
+	// for문 4회전때는 3회전때와 마찬가지로 j가 감소하며 while문 반복실행
+		// 3회전 때와의 차이점 while문의 조건부 중에 전자 조건부가 아닌 
+		// 후자 조건부(array[j-1]>array[j])로 인해 j가 2까지 갔을 때 while문 break;
+		// 4회전때 j가 2일 경우 후자 조건부 => 2 > 4 이므로 실행X
+	// for문 4회전 while문 전체 실행 후 -> array[]={2,4,5,6,8}	
+		
+		
+	// 의문점...선택정렬과 무엇이 다른가....
 
 
 	}
 }
+
 
 
 
@@ -60,7 +86,7 @@ public class 삽입정렬 {
                 i--;
             }
 
-            // 삽입될 위치에 tmp 삽입
+            // 삽입될 위치에 temp 삽입
             array[i] = temp;
         }
        for(int i=0; i<array.length; i++) {
